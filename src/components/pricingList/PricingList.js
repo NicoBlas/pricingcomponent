5eb0b82099a6a3f6bb0fcfaf5c75e8cb702b4ca0
Nico Blasutig <nicolasblasutig@gmail.com>
2022-03-17 14:59:39 -0300

    views done

--- src/components/pricingList/PricingList.js
+++ src/components/pricingList/PricingList.js
@@ -7,31 +7,34 @@ const data = [{
   price: "19.99",
   storage: "500 GB Storage",
   users: "2 Users Allowed",
-  gb: "Send up to 3 GB"
+  gb: "Send up to 3 GB",
+  mid: false
 
 },
 {
-  pack: "Proffesional",
+  pack: "Professional",
   price: "24.99",
   storage: "1 TB Storage",
   users: "5 Users Allowed",
-  gb: "Send up to 10 GB"
+  gb: "Send up to 10 GB",
+  mid: true
 },
 {
   pack: "Master",
   price: "39.99",
   storage: "2 TB Storage",
   users: "10 Users Allowed",
-  gb: "Send up to 20 GB"
+  gb: "Send up to 20 GB",
+  mid:false
 }
 ]
 
 const PricingList = () => {
   return (
     <div className='pricingList'>
-      <Pricing pack = {data[0].pack} price = {data[0].price} storage= {data[0].storage} users = {data[0].users} gb = {data[0].gb}/>
-      <Pricing className="pricingMid" pack = {data[1].pack} price = {data[1].price} storage= {data[1].storage} users = {data[1].users} gb = {data[1].gb}/>
-      <Pricing pack = {data[2].pack} price = {data[2].price} storage= {data[2].storage} users = {data[2].users} gb = {data[2].gb}/>
+      <Pricing mid={data[0].mid} pack = {data[0].pack} price = {data[0].price} storage= {data[0].storage} users = {data[0].users} gb = {data[0].gb}/>
+      <Pricing mid={data[1].mid} pack = {data[1].pack} price = {data[1].price} storage= {data[1].storage} users = {data[1].users} gb = {data[1].gb}/>
+      <Pricing mid={data[2].mid} pack = {data[2].pack} price = {data[2].price} storage= {data[2].storage} users = {data[2].users} gb = {data[2].gb}/>
 
     </div>
   )
